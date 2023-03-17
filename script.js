6dfe944ebc02261379da96bfd47b8382ee2dee8f
levon-dalakyan <wman8800@gmail.com>
2023-03-17 17:57:19 +0300

    Refactoring

--- script.js
+++ script.js
@@ -218,17 +218,3 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.value = inputClosePin.value = '';
     inputClosePin.blur();
 });
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
